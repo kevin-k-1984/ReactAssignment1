4f7191f4624a47f81fc7979f9ee639e626909902
kevin_k <kkemp1984@gmail.com>
2021-06-10 22:05:12 -0500

    formating

--- src/redux/ReducerComments.js
+++ src/redux/ReducerComments.js
@@ -8,7 +8,7 @@ export const Comments = (state = { errMess: null, comments: [] }, action) => {
             return { ...state, isLoading: false, errMess: action.payload, comments: [] };
         case ActionTypes.ADD_COMMENT:
             var comment = action.payload;
-            return {...state, comments: state.comments.concat(comment)};
+            return { ...state, comments: state.comments.concat(comment) };
         default:
             return state;
     }
